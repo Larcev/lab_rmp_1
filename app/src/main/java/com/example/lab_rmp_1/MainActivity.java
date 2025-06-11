b3690f1294bc7b5139ee6456cd5026fcd70940c0
Larcev <rustamtastemiroff@yandex.ru>
2025-06-11 22:40:20 +0600

    Добавлен фрагмент в контейнер Activity

--- app/src/main/java/com/example/lab_rmp_1/MainActivity.java
+++ app/src/main/java/com/example/lab_rmp_1/MainActivity.java
@@ -2,11 +2,21 @@ package com.example.lab_rmp_1;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            BlankFragment blankFragment = new BlankFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.add(R.id.fragment_container, blankFragment);
+            transaction.commit();
+        }
     }
 }
